0c829fcb76d22f45c62c039e46d2dc6b8c27149c
akeirn <ally.keirn@gmail.com>
2022-12-02 13:42:59 -0500

    First pass state machine

--- fpga1/src/sd_state_machine.sv
+++ fpga1/src/sd_state_machine.sv
@@ -1,17 +1,20 @@
 `timescale 1ns / 1ps
 `default_nettype none
 
-module song_selection(input wire clk_25mhz,
-                    input wire sd_cd,
+module sd_state_machine(input wire clk_25mhz,
+                    input wire sd_cd, //ignore piped in from top level
                     input wire rst, 
-                    input wire play_button,
+                    input wire read_signal, //Signal to start reading a 512 byte block
+                    input wire [31:0] address,
 
-                    inout wire [3:0] sd_dat,
+                    inout wire [3:0] sd_dat, //sd_data, used for read/write. Ignore for our application
 
-                    output logic [7:0] data_out, 
+                    output logic [7:0] data_out, //Byte of data read out
+                    output logic data_valid, //Goes high for one clock cycle to prevent multiple reads of the same byte. 
+                    output logic done, //Goes high at the end of a 512 block. 
                     output logic sd_reset, 
-                    output logic sd_sck, 
-                    output logic sd_cmd
+                    output logic sd_sck, // Sd card has its own clock
+                    output logic sd_cmd //Ignore not used
                     
     );
     
@@ -23,7 +26,6 @@ module song_selection(input wire clk_25mhz,
     logic rd;                   // read enable
     logic wr;                   // write enable
     logic [7:0] din;            // data to sd card
-    logic [31:0] addr = 0;          // starting address for read/write operation
 
     assign wr = 1'b0;
     assign din = 2'b00;
@@ -38,13 +40,13 @@ module song_selection(input wire clk_25mhz,
     // control variables
     logic [19:0] byte_count; //What byte are we reading from
     logic prev_byte_available; //track rising edge of byte available
-    typedef enum {INIT, WAIT, PLAYING} states;
+    typedef enum {INIT, WAIT, PLAYING} states; //State machine for reading
     states state = WAIT;
-    parameter byte_target = 2'b10;
+    parameter byte_target = 2'b10; //Can be used for debugging to specify a specific byte in playing if block. 
     
     // handles reading from the SD card
-    sd_controller sd(.reset(rst), .clk(clk_25mhz), .cs(sd_dat[3]), .mosi(sd_cmd), 
-                     .miso(sd_dat[0]), .sclk(sd_sck), .ready(ready), .address(addr),
+    sd_controller sd(.reset(rst), .clk(clk_25mhz), .cs(sd_dat[3]), .mosi(sd_cmd),
+                     .miso(sd_dat[0]), .sclk(sd_sck), .ready(ready), .address(address),
                      .rd(rd), .dout(dout), .byte_available(byte_available),
                      .wr(wr), .din(din), .ready_for_next_byte(ready_for_next_byte), .status(status)); 
 
@@ -52,16 +54,19 @@ module song_selection(input wire clk_25mhz,
         prev_byte_available <= byte_available;
 
         if(rst) begin
-            addr <= 0;
             rd <= 1'b0;
             data_out <= 8'b11111111; //Set this default so it's obvious it hasn't read
             state <= INIT;
             byte_count <= 0;
+            data_valid <= 1'b0;
+            done <= 1'b0;
         end else begin
             case(state)
                 INIT: begin //Wait for button press to read audio
                     rd <= 1'b0;
-                    if (play_button) state <= WAIT;
+                    data_valid <= 1'b0;
+                    done <= 1'b0;
+                    if (read_signal) state <= WAIT;
                 end
                 WAIT: begin
                     if(ready) begin //Wait for SD ready signal to begin read
@@ -73,14 +78,15 @@ module song_selection(input wire clk_25mhz,
                 PLAYING: begin //Read 512 bytes of audio data
                     rd <= 1'b0;
  
-                    if(byte_available & !prev_byte_available) begin
-                        if (byte_count == byte_target) begin
-                            data_out <= dout;
-                        end
-
+                    if(byte_available & !prev_byte_available) begin //only read each byte once. 
+                        data_out <= dout;
+                        data_valid <= 1'b1;
                         byte_count <= byte_count + 1;
                     end else if(byte_count >= 512) begin
                         state <= INIT;
+                        done <= 1'b1;
+                    end else begin
+                        data_valid <= 1'b0;
                     end
                 end
             endcase
--- fpga1/src/top_level.sv
+++ fpga1/src/top_level.sv
@@ -27,19 +27,39 @@ module top_level(input wire clk_100mhz,
     logic empty;
     logic [7:0] fifo_dout;
     logic rd_en; 
-    logic srst;
+    logic empty_flag;
 
-    typedef enum {INIT, WRITE, READ} states;
+    //Sd values
+    parameter [31:0] song0_addr_start = 0;
+    parameter [31:0] song0_addr_end = 'd512; //change later
+    parameter [31:0] song1_addr_start = 0; //change later
+    parameter [31:0] song1_addr_end = 0;
+    parameter [31:0] song2_addr_start = 0; //change later
+    parameter [31:0] song2_addr_end = 0;
+
+    logic read_block; //read a 512 byte block from sd card
+    logic sd_data_valid; //valid data on the sd card putup
+    logic sd_done; //Goes high when the sd card is done reading a block (for 1 clock cycle)
+    logic [31:0] current_addr = song0_addr_start;
+    logic [31:0] end_addr = song0_addr_end;
+
+
+
+    //Top level state machine
+    typedef enum {WAIT, FIRST_BLOCK, TRANSMIT} states;
     states state;
+
+    logic [2:0] song_num; //song number for keeping track of current selection (used for VGA and addressing)
+    logic select_song; //To select a song to play
+    logic up_button; //up a song in the menu (vga)
+    logic down_button; //down a song in the menu (vga)
+    logic prev_frame_ready; //keep track of frame read to find rising edge
+    logic sd_busy; //used to track if the sd card is currently pulling a block
+
 
     // Clocks
     logic clk_25mhz;
     logic clk_buff_100mhz;
-
-    // Combinational logic 
-    assign rst = btnr; 
-    assign din = 8'b11011111; 
-    assign srst = 0; 
 
     // generate 25 mhz clock for sd_controller 
     clk_100mhz_25mhz clock_gen1(.clk_in1(clk_100mhz), .clk_out1(clk_25mhz),.clk_out2(clk_buff_100mhz));
@@ -48,13 +68,32 @@ module top_level(input wire clk_100mhz,
     // logic clk_6144mhz;
     // clk_100mhz_6144mhz clock_gen2(.clk_in1(clk_100mhz), .clk_out1(clk_6144mhz));
 
-    song_selection ss(.clk_25mhz(clk_25mhz),
+    //Debouncers for selection buttons
+    debouncer btnc_db(.clk_in(clk_25mhz),
+                  .rst_in(rst),
+                  .dirty_in(btnc),
+                  .clean_out(select_song));
+
+    debouncer btnu_db(.clk_in(clk_25mhz),
+                  .rst_in(rst),
+                  .dirty_in(btnc),
+                  .clean_out(up_button));
+
+    debouncer btnd_db(.clk_in(clk_25mhz),
+                  .rst_in(rst),
+                  .dirty_in(btnc),
+                  .clean_out(down_button));              
+
+    sd_state_machine ss(.clk_25mhz(clk_25mhz),
                     .sd_cd(sd_cd),
                     .rst(rst), 
-                    .play_button(btnc), //used to read from sd card
+                    .read_signal(read_block), //used to read from sd card
+                    .address(current_addr), //address to read at for sd card. Must be a multiple of 512 bytes. 
                     .sd_dat(sd_dat),
 
                     .data_out(data_out), //one byte audio data
+                    .data_valid(sd_data_valid), //data is new and valid (high for one clock cycle)
+                    .done(sd_done),
                     .sd_reset(sd_reset), 
                     .sd_sck(sd_sck), 
                     .sd_cmd(sd_cmd)
@@ -62,59 +101,99 @@ module top_level(input wire clk_100mhz,
 
     seven_segment_controller sev(.clk_in(clk_buff_100mhz),
                                 .rst_in(rst),
-                                .val_in(fifo_dout),
+                                .val_in({5'b0, song_num, 6'b0, state}),
                                 .cat_out({cg, cf, ce, cd, cc, cb, ca}),
                                 .an_out(an));
 
     fifo_generator_0 fif0(.full(full), .din(din), .wr_en(wr_en), 
                          .empty(empty), .dout(fifo_dout), .rd_en(rd_en), 
-                         .clk(clk_25mhz), .srst(srst));
+                         .clk(clk_25mhz), .srst(rst), .prog_empty(empty_flag));
 
-    // fifo buffer
-    // logic [19:0] frame_dout;
-    // fifo_generator_1 fif1(.full(full), .din(fifo_dout), .wr_en(wr_en), 
-    //                      .empty(empty), .dout(frame_dout), .rd_en(ready), 
-    //                      .clk(clk_25mhz), .srst(srst));
-
-    // frame assembbly
+    // frame assembly
     // logic ready;
     // logic data_bit;
     // frame_assembly transmission(.clk(clk_6144mhz), .rst(btnr), .din(frame_dout), .ready(ready), .dout(data_bit));
+  
+    // Combinational logic 
+    assign rst = btnr; 
 
-    /* ----------------------------------------------------------------------------
-     * TO-DO LIST:
-     *  - FIFO Buffer: we need a buffer between the first FIFO and the frame 
-          assembler in order to allow for information to be always available
+    //set up the write signals for FIFOs. 
+    assign wr_en = sd_data_valid & !full; //if it's not full, there's valid sd data, write
+    assign din = data_out;
 
-     *  - Frame Assembly/Transmitter: we need a robust frame assembler that
-          gathers data from the FIFO Buffer and constructs the preamble,
-          data, and other flags and sends them out continuously
-     * ----------------------------------------------------------------------------
-    */
-
-
-    
     always_ff @(posedge clk_25mhz) begin
+        prev_frame_ready <= frame_ready; //to account for slower transmit clock cycle look for rising edge
+
         if(rst) begin
-            rd_en <= 0;
-            wr_en <= 0;
-            state <= INIT;
+            state <= WAIT;
+            song_num <= 2'b00;
+            read_block <= 1'b0;
+            rd_en <= 1'b0;
         end else begin
-            case(state) 
-                INIT: begin
-                    rd_en <= 0;
-                    wr_en <= 1;
-                    state <= WRITE;
+            case(state)
+                WAIT: begin
+                    //ensure fifo is reset
+                    rd_en <= 1'b0;
+
+                    if(select_song) begin
+                        state <= FIRST_BLOCK;
+                        read_block <= 1'b1;
+
+                        //Select the correct addresses
+                        case(song_num)
+                            0: begin
+                                current_addr <= song0_addr_start;
+                                end_addr <= song0_addr_end;
+                            end
+                            1: begin
+                                current_addr <= song1_addr_start;
+                                end_addr <= song1_addr_end;
+                            end
+                            2: begin
+                                current_addr <= song2_addr_start;
+                                end_addr <= song2_addr_end;
+                            end
+                            default begin
+                                current_addr <= song0_addr_start;
+                                end_addr <= song0_addr_end; 
+                            end
+                        endcase
+
+                    //Display goes to playing screen
+                    end else if(up_button) begin
+                        song_num <= song_num - 1;
+                    end else if(down_button) begin
+                        song_num <= song_num + 1;
+                    end
+
                 end
-                WRITE: begin
-                    wr_en <= 0;
-                    rd_en <= 1;
-                    state <= READ;
+
+                FIRST_BLOCK: begin
+                    read_block <= 1'b0; //reset read signal
+
+                    if(sd_done) begin //done loading a block, ready for transmit
+                        fifo_ready <= 1'b1;
+                        current_addr <= current_addr + 4096;
+                        state <= TRANSMIT;
+                    end 
                 end
-                READ: begin
-                    rd_en <= 0;
-                    state <= INIT;
-                end
+
+                TRANSMIT: begin
+                    if(current_addr > end_addr) begin //Done with the song. 
+                        state <= WAIT;
+                        fifo_ready <= 1'b0; //reset to alert module
+                    end else begin
+                        if(empty_flag & !sd_busy) begin //check if we hav 512 bytes available in FIFO. If so, start a new write
+                            read_block <= 1'b1;
+                            sd_busy <= 1'b1;
+                        end else if(sd_done) begin //SD finished, change busy flag to allow for next cycle
+                            sd_busy <= 1'b0;
+                            read_block <= 1'b0;
+                        end else begin //No space, or sd is busy writing. 
+                            read_block <= 1'b0;
+                        end  
+                    end
+                end 
             endcase
         end
     end
