c6fb8b989d2c17503d4245296f1951cf33370646
Louis Martinez <luismapena15@gmail.com>
2022-12-04 15:28:53 -0500

    mmmm

--- fpga2/src/receiver.sv
+++ fpga2/src/receiver.sv
@@ -3,18 +3,40 @@
 
 module receiver(input wire clk, 
                 input wire rst, 
-                input wire din,
+                input wire [7:0] din,
                 
-                output wire dout,
-                output wire vout);
+                output logic dout,
+                output logic vout);
 
+    logic [8:0] counter0;
+    logic [8:0] counter1;
     logic prev; 
     always_ff@(posedge clk) begin
         if (rst) begin
             dout <= 0;
             vout <= 0;
         end else begin
-            // Do sample stuff
+            if (din == 8'b1111_1111) begin
+                if (counter0 == 0) begin
+                    counter1 <= counter1 + 1;
+                    vout <= 0;
+                end else begin
+                    counter1 <= counter1 + 1;
+                    counter0 <= 0;
+                    vout <= 1;
+                    dout <= 0;
+                end
+            end else begin
+                if (counter1 == 0) begin
+                    counter0 <= counter0 + 1;
+                    vout <= 0;
+                end else begin
+                    counter0 <= counter0 + 1;
+                    counter1 <= 0;
+                    vout <= 1;
+                    dout <= 1;
+                end
+            end
         end
     end
 
--- fpga2/src/top_level.sv
+++ fpga2/src/top_level.sv
@@ -2,28 +2,11 @@
 `default_nettype none
 
 module top_level(input wire clk_100mhz, 
-                 input wire btnr,
-                 input logic jc[0],
+                 input wire btnc,
+                 input wire [7:0] jc,
 
                  output logic [7:0] ja
     );
-    
-    logic rst;            
-    assign rst = btnr; 
-
-    logic clk_6144mhz;
-    clk_wiz_2 spdif_clock(.clk_in1(clk_100mhz), .clk_out1(clk_6144mhz));
-    assign ja = {clk_6144mhz, clk_6144mhz, clk_6144mhz, clk_6144mhz, clk_6144mhz, clk_6144mhz, clk_6144mhz, clk_6144mhz};
-
-    logic clk_60mhz;
-    clk_wiz_60mhz receiver_clock(.clk_in1(clk_100mhz), .clk_out1(clk_60mhz));
-
-    // receiver receiver_hardware();
-    // biphasemark_decoder receiver_bmc();
-    // frame_dismantle receiver_frame();
-    // fifo fpga2_fifo();
-    // convolution fpga2_convolution();
-    // frame_assemble fpga2_transmitter();
 
     /* ///////////////////////////////////////////////////
     TO-DO LIST:
@@ -31,6 +14,26 @@ module top_level(input wire clk_100mhz,
     2. Audio Manipulation (convolution)
     3. Send through transmission circuit
     */ ///////////////////////////////////////////////////
+    
+    logic rst;            
+    assign rst = btnc; 
+
+    logic clk_6144mhz;
+    clk_wiz_6144mhz_clk_wiz spdif_clock(.clk_in1(clk_100mhz), .clk_out1(clk_6144mhz));
+    assign ja = {clk_6144mhz, clk_6144mhz, clk_6144mhz, clk_6144mhz, clk_6144mhz, clk_6144mhz, clk_6144mhz, clk_6144mhz};
+
+    logic clk_60mhz;
+    clk_wiz_60mhz_clk_wiz receiver_clock(.clk_in1(clk_100mhz), .clk_out1(clk_60mhz));
+
+    logic dout;
+    logic vout;
+    receiver our_receiver(.clk(clk_60mhz), .rst(btnc), .din(jc), .dout(dout), .vout(vout));
+
+    // biphasemark_decoder receiver_bmc();
+    // frame_dismantle receiver_frame();
+    // fifo fpga2_fifo();
+    // convolution fpga2_convolution();
+    // frame_assemble fpga2_transmitter();
     
 endmodule
 
